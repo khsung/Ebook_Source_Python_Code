5b8fe3ced71913e5131ebec8621ebe6caa520323
khsung <khsung0@nate.com>
2020-11-30 16:30:56 +0900

    큐

---
+++ Ebook_Source_Python_Code/Queue.py
@@ -0,0 +1,27 @@
+
+class Queue(list):   #큐 클래스 선언
+    def __init__(self):   #스택 리스트로 초기화
+        self.data=[]
+
+    def isempty(self):    #큐 공백 상태 체크
+        return not self.data
+
+    def enqueue(self, data):  #리스트 뒤에 원소 삽입
+        self.data.append(data)
+
+    def dequeue(self):     #리스트 앞 원소 제거
+        if self.isempty()==True:
+            print("공백 큐")
+        else:
+            self.data.pop(0)
+
+    def printqueue(self):  #큐 출력
+        print(self.data)
+
+queue=Queue()
+queue.dequeue()
+queue.enqueue(10)
+queue.enqueue(20)
+queue.printqueue()
+queue.dequeue()
+queue.printqueue()
