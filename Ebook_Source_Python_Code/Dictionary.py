acaac29a85df01108424e152d8f691bda9e1363a
khsung <khsung0@nate.com>
2020-12-01 16:56:12 +0900

    딕셔너리

---
+++ Ebook_Source_Python_Code/Dictionary.py
@@ -0,0 +1,10 @@
+
+dic={0:'AAA',1:'BBB',2:'CCC'}
+print(dic)
+print(dic.keys())  #키 값 출력
+print(dic.values()) #데이터 값 출력
+print(dic[1])  #키 값 1 인 데이터 출력
+dic['DDD']=3 #키 값 'DDD'인 데이터 3 추가
+del dic[2]   #키 값 2인 데이터 삭제
+print(dic)
+
