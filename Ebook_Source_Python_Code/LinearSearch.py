d9c948f596b05bd3cb0440467fe0b428445bfbfd
khsung <khsung0@nate.com>
2020-12-01 09:35:34 +0900

    선형 검색

---
+++ Ebook_Source_Python_Code/LinearSearch.py
@@ -0,0 +1,11 @@
+
+answer=6      #찾을 원소
+check=False   #원소가 있는지 체크하는 변수
+intlist=[1,2,3,4,5,6,7,8,9,10]
+for i in range(0,len(intlist)):   #첫 원소부터 끝까지 검색
+    if intlist[i]==answer:        #리스트에서 원하는 원소를 찾는 코드
+        print("검색 완료 :",intlist[i],"인덱스 :",i)
+        check=True
+if check==False:            #원소가 없을 경우
+    print("해당 원소 없음")
+
