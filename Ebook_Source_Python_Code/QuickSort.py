4c46ad883229b8e19bf63516579ed5a4cd4c8a2d
khsung <khsung0@nate.com>
2020-12-04 11:36:56 +0900

    퀵 정렬

---
+++ Ebook_Source_Python_Code/QuickSort.py
@@ -0,0 +1,33 @@
+def quicksort(arr):
+    if len(arr)<2:          #리스트의 크기가 1일 때
+        return arr          #리스트 반환
+    else:
+        pivot=0             #첫 번째 원소 pivot설정
+                            #원하는 원소를 정해도 됨
+        left=1              #pivot 다음 원소
+        right=len(arr)-1    #리스트 끝 원소
+        print("Pivot : ",arr[pivot])
+        print(arr,"->")
+        while left<right:   #left와 right가 엇갈릴 때까지 반복
+            if arr[left]<arr[pivot]:
+                left+=1
+            elif arr[right]>arr[pivot]:
+                right-=1
+            else:      #left가 pivot보다 크고 right가 
+                       #pivot보다 작을 경우 교환
+                arr[left],arr[right]=arr[right],arr[left]
+
+        #첫 번째 원소를 pivot으로 정했으므로 
+        #pivot보다 작은 원소들 중 가장
+        #오른쪽에 있는 원소와 pivot의 값 교환
+        if arr[left]<arr[pivot]:
+            arr[left],arr[pivot]=arr[pivot],arr[left]
+        else:
+            arr[left-1],arr[pivot]=arr[pivot],arr[left-1]
+        print(arr)
+        return quicksort(arr[:left])+quicksort(arr[right:])
+
+arr=[5,1,3,7,6,2,4]
+print("정렬 전 배열 : ",arr)
+print("\n정렬 과정")
+print("\n정렬 된 배열 : ",quicksort(arr))
