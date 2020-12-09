42d94a98269f04f878bf0932e2d5f75c1fb5cfaf
khsung <khsung0@nate.com>
2020-12-09 17:20:36 +0900

    이진 트리

--- Ebook_Source_Python_Code/BinaryTree.py
+++ Ebook_Source_Python_Code/BinaryTree.py
@@ -3,7 +3,7 @@ class Btree(object):
         self.data=None;
         self.left=self.right=None;
     
-    def insert(self,data):
+    def insertnode(self,data):
         if self.data==None:
             self.data=data
         else:
@@ -11,13 +11,51 @@ class Btree(object):
                 if self.right==None:
                     newnode=Btree()
                     self.right=newnode
-                    newnode.insert(data)
+                    newnode.insertnode(data)
+                else:
+                    self=self.right
+                    self.insertnode(data)
+            else:
+                if self.left==None:
+                    newnode=Btree()
+                    self.left=newnode
+                    newnode.insertnode(data)
+                else:
+                    self=self.left
+                    self.insertnode(data)
 
+    def deletenode(self,data):
+        if tree.data==None:
+            print("공백 이진 트리")
+        elif self.data<data:
+            if self.right==None:
+                print(data,"에 해당하는 원소 없음")
+            else:
+                parent=self
+                self=self.right
+                self.deletenode(data)
+        elif self.data>data:
+            if self.left==None:
+                print(data,"에 해당하는 원소 없음")
+            else:
+                parent=self
+                self=self.left
+                self.deletenode(data)
+        #else:
+        #    if curr.left==None and curr.right==None:
+        #        if parent==None
 
     def printnode(self):
         print(self.data)
         print(self.right.data)
+        print(self.left.data)
+        print(self.left.right.data)
+
+
 tree=Btree()
-tree.insert(1)
-tree.insert(2)
+tree.insertnode(3)
+tree.insertnode(4)
+tree.insertnode(1)
+tree.insertnode(2)
 tree.printnode()
+tree.deletenode(100)
