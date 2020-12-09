bbbf46d55f30be4dc998a028a0acc693cd93ec27
khsung <khsung0@nate.com>
2020-12-09 15:35:14 +0900

    이진 트리

--- Ebook_Source_Python_Code/BinaryTree.py
+++ Ebook_Source_Python_Code/BinaryTree.py
@@ -1,6 +1,23 @@
-class Btree:
+class Btree(object):
     def __init__(self):
         self.data=None;
         self.left=self.right=None;
+    
+    def insert(self,data):
+        if self.data==None:
+            self.data=data
+        else:
+            if self.data<data:
+                if self.right==None:
+                    newnode=Btree()
+                    self.right=newnode
+                    newnode.insert(data)
 
-B=Btree()
+
+    def printnode(self):
+        print(self.data)
+        print(self.right.data)
+tree=Btree()
+tree.insert(1)
+tree.insert(2)
+tree.printnode()
