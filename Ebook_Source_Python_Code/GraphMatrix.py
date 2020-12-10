477c0fa74ac5252707909f65d3e1ff2a0686f273
khsung <khsung0@nate.com>
2020-12-10 16:37:28 +0900

    인접 행렬 그래프

---
+++ Ebook_Source_Python_Code/GraphMatrix.py
@@ -0,0 +1,17 @@
+
+def printmatrix(self):
+    for i in range(0,len(self)):
+        print(self[i],end="\n\n")
+    print()
+
+undirected=[[0,1,1,0],[1,0,0,1],[1,0,0,1],[0,1,1,0]]
+directed=[[0,1,1,0],[0,0,0,1],[0,0,0,1],[0,1,1,0]]
+weighted=[[0,7,2,0],[7,0,0,5],[2,0,0,10],[0,5,10,0]]
+
+print("무 방향 그래프\n")
+printmatrix(undirected)
+print("방향 그래프\n")
+printmatrix(directed)
+print("가중치 그래프\n")
+printmatrix(weighted)
+
