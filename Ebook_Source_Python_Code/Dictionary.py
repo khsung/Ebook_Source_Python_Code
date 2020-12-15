d5032d308768458ead39556589917ebf0b86d573
khsung <khsung0@nate.com>
2020-12-15 16:31:53 +0900

    딕셔너리(해시 검색)

--- Ebook_Source_Python_Code/Dictionary.py
+++ Ebook_Source_Python_Code/Dictionary.py
@@ -1,10 +1,12 @@
 
 dic={0:'AAA',1:'BBB',2:'CCC'}
-print(dic)
+print("딕셔너리 : ",dic)
 print(dic.keys())  #키 값 출력
 print(dic.values()) #데이터 값 출력
-print(dic[1])  #키 값 1 인 데이터 출력
+
+#키 값 1 인 데이터 출력
+print("키 값 1인 데이터 :",dic[1])
 dic['DDD']=3 #키 값 'DDD'인 데이터 3 추가
 del dic[2]   #키 값 2인 데이터 삭제
-print(dic)
+print("딕셔너리 : ",dic)
 
