8f023babb0cb4fdc9167a7ce794c2818670726fc
khsung <khsung0@nate.com>
2020-12-20 23:04:54 +0900

    백트래킹(D찾기)

--- Ebook_Source_Python_Code/Backtracking(find D).py
+++ Ebook_Source_Python_Code/Backtracking(find D).py
@@ -6,12 +6,23 @@ class Tree(object):
 
         #방문한 노드인지
         #체크하는 변수
-        self.visited=0
-        self.Lchild=None
-        self.Rchild=None
+        self.visited = 0
+        self.Lchild = None
+        self.Rchild = None
 
-#def backtrackingFindAnswer(self):
-
+def backtrackingFindAnswer(self):
+    print("현재 노드의 데이터 : "+self.data)
+    if self.data == answer:
+        print("데이터 "+self.data+" 찾음")
+    else:
+        #왼쪽 자식노드를 방문 안 했을 경우 왼쪽 자식으로 이동
+        if self.Lchild != None and self.Lchild.visited == 0:
+            backtrackingFindAnswer(self.Lchild)
+        #오른쪽 자식노드를 방문 안 했을 경우 오른쪽 자식으로 이동
+        if self.Rchild != None and self.Rchild.visited == 0:
+            backtrackingFindAnswer(self.Rchild)
+        #현재 노드를 방문한 노드로 처리
+        self.visited = 1
 
 #백트래킹을 위한 예제이므로
 #단순한 트리구조 생성
@@ -19,8 +30,10 @@ Anode=Tree('A')
 Bnode=Tree('B')
 Cnode=Tree('C')
 Dnode=Tree('D')
+
 answer='D'
 Anode.Lchild=Bnode   #   A
 Bnode.Lchild=Cnode   #  B
 Bnode.Rchild=Dnode   # C D
 
+backtrackingFindAnswer(Anode)
