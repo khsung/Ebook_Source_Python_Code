a99f75ea6f92674388ccdc085f98ad428a12b40d
khsung <khsung0@nate.com>
2020-12-20 22:54:04 +0900

    백트래킹 미완성

--- Ebook_Source_Python_Code/Backtracking(find D).py
+++ Ebook_Source_Python_Code/Backtracking(find D).py
@@ -1,1 +1,26 @@
-#트리에서 D찾는 코드 구현
+
+#트리 방문 백트래킹 예제
+class Tree(object):
+    def __init__(self,data):
+        self.data=data
+
+        #방문한 노드인지
+        #체크하는 변수
+        self.visited=0
+        self.Lchild=None
+        self.Rchild=None
+
+#def backtrackingFindAnswer(self):
+
+
+#백트래킹을 위한 예제이므로
+#단순한 트리구조 생성
+Anode=Tree('A')
+Bnode=Tree('B')
+Cnode=Tree('C')
+Dnode=Tree('D')
+answer='D'
+Anode.Lchild=Bnode   #   A
+Bnode.Lchild=Cnode   #  B
+Bnode.Rchild=Dnode   # C D
+
