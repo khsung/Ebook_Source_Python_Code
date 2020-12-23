880ea74720dd776fe5473052644f661bbfbe0540
khsung <khsung0@nate.com>
2020-12-23 17:49:32 +0900

    이진트리순회

--- Ebook_Source_Python_Code/BinaryTree.py
+++ Ebook_Source_Python_Code/BinaryTree.py
@@ -1,86 +1,155 @@
+
 class Btree(object):
+    #데이터와 두 자식노드 초기화
     def __init__(self):
         self.data=None;
         self.left=self.right=None;
-        self.parent=self;
+
+    #노드 삽입
     def insertnode(self,data):
+        #현재노드가 비어있을 경우
         if self.data==None:
             self.data=data
         else:
+            #현재 노드 데이터보다 클 때
             if self.data<data:
+                #오른쪽 자식이 없을 경우 삽입
                 if self.right==None:
                     newnode=Btree()
                     self.right=newnode
                     newnode.insertnode(data)
+
+                #오른쪽 자식이 있을 경우 이동
                 else:
                     self=self.right
                     self.insertnode(data)
+
+            #현재 노드 데이터보다 같거나 작을 때
             else:
+                #왼쪽 자식이 비어있을 경우 삽입
                 if self.left==None:
                     newnode=Btree()
                     self.left=newnode
                     newnode.insertnode(data)
                     self.parent=newnode
+                #왼쪽 자식이 있을 경우 이동
                 else:
                     self=self.left
                     self.insertnode(data)
 
-    #def deletenode(self,data):
-    #    if tree.data==None:
-    #        print("공백 이진 트리")
-    #    elif self.data<data:
-    #        if self.right==None:
-    #            print(data,"에 해당하는 원소 없음")
-    #        else:
-    #            self.parent=self
-    #            self=self.right
-    #            self.deletenode(data)
-    #    elif self.data>data:
-    #        if self.left==None:
-    #            print(data,"에 해당하는 원소 없음")
-    #        else:
-    #            self.parent=self
-    #            self=self.left
-    #            self.deletenode(data)
-    #    else:
-    #        if self.left==None and self.right==None:
-    #            if self.parent.right==self:
-    #                self.parent.right=None
-    #            elif self.parent.left==self:
-    #                self.parent.left=None
-    #            else:
-    #                self.data=None;
-    #        elif self.left==None:
-    #            temp=self
-    #            self.parent=self
-    #            self=self.right
-    #            if self.left==None:
-    #                temp.data,self.data=self.data,temp.data
-    #                self.parent.right=None
-    #                del(self)
-    #            else:
-    #                while self.left!=None:
-    #                    self.parent=self
-    #                    self=self.left
-    #                temp.data,self.data=self.data,temp.data
-    #                self.parent.left=None
-    #                del(self)
-    #        else:
-    #            temp=self
-    #            self.parent=self
-    #            self=self.left
-    #            if self.right==None:
-    #                temp.data,self.data=self.data,temp.data
-    #                self.parent.left=None
-    #                del(self)
-    #            else:
-    #                while self.right!=None:
-    #                    self.parent=self
-    #                    self=self.right
-    #                temp.data,self.data=self.data,temp.data
-    #                self.parent.right=None
-    #                del(self)
+    #노드 삭제
+    def deletenode(self,data):
+        #현재 노드의 부모노드를 저장하기 위한 전역변수
+        global parent
 
+        #루트노드가 비어있을 경우 오류 출력
+        if tree.data==None:
+            print("공백 이진 트리")
+
+        #현재 노드의 데이터보다 삭제할 데이터가 클 경우
+        elif self.data<data:
+            #오른쪽 자식이 없을 경우 오류출력
+            if self.right==None:
+                print(data,"에 해당하는 원소 없음\n")
+
+            #현재 노드를 부모노드로 저장하고
+            #오른쪽 자식으로 이동
+            else:
+                parent=self
+                self=self.right
+                self.deletenode(data)
+
+        #현재 노드의 데이터보다 삭제할 데이터가 작을 경우
+        elif self.data>data:
+            #왼쪽 자식이 없을 경우 오류출력
+            if self.left==None:
+                print(data,"에 해당하는 원소 없음\n")
+            
+            #현재 노드를 부모노드로 저장하고
+            #오른쪽 자식으로 이동
+            else:
+                parent=self
+                self=self.left
+                self.deletenode(data)
+
+        #현재 노드가 지워야 할 노드일 경우
+        else:
+            #자식노드가 없을 경우
+            if self.left==None and self.right==None:
+                #부모의 오른쪽 자식노드가 현재 노드일 경우
+                #오른쪽 자식노드의 연결을 끊어줌
+                if parent.right==self:
+                    parent.right=None
+
+                #부모의 왼쪽 자식노드가 현재 노드일 경우
+                #왼쪽 자식노드의 연결을 끊어줌
+                elif parent.left==self:
+                    parent.left=None
+
+                #둘다 아닐 경우는 루트 노드밖에없음
+                #따라서 노드삭제가 아닌 데이터만 삭제
+                else:
+                    self.data=None;
+
+            #오른쪽 자식만 있을 경우 부모노드로 현재노드를
+            #저장하고 오른쪽 자식으로 이동
+            elif self.left==None:
+                temp=self
+                parent=self
+                self=self.right
+
+                #현재노드의 왼쪽 자식이 비어 있을 경우
+                #현재노드와 원래 지워야 할 노드의 데이터를
+                #교환 후 현재노드의 오른쪽 자식을 지워야 할
+                #노드의 오른쪽 자식으로 연결해준다
+                if self.left==None:
+                    temp.data,self.data=self.data,temp.data
+                    parent.right=self.right
+                    del(self)
+
+                #현재노드의 왼쪽 자식이 있을 경우 최대한
+                #왼쪽자식으로 이동후 지워야 할 노드의 데이터와
+                #교환하고 노드를 삭제한다
+                #즉, 지워야 할 노드보다 큰 데이터 중 가장 작은
+                #데이터로 교환하는 것을 뜻한다.
+                else:
+                    while self.left!=None:
+                        parent=self
+                        self=self.left
+                    temp.data,self.data=self.data,temp.data
+                    parent.left=None
+                    del(self)
+
+            #왼쪽 자식이 있을 경우 부모노드로 현재노드를 
+            #저장하고 왼쪽 자식으로 이동
+            else:
+                temp=self
+                parent=self
+                self=self.left
+
+                #현재노드의 오른쪽 자식이 없을 경우
+                #현재노드와 원래 지워야 할 노드의 데이터를
+                #교환한 후 원래노드의 왼쪽 자식에 현재노드의
+                #왼쪽 자식노드를 연결해준다
+                if self.right==None:
+                    temp.data,self.data=self.data,temp.data
+                    parent.left=self.left
+                    del(self)
+
+                #오른쪽 자식이 있을 경우
+                else:
+                    #오른쪽 자식이 없을 때까지 현재노드를
+                    #부모노드로 저장하고 오른쪽으로 이동한 뒤
+                    #지워야 할 노드의 데이터와 교환하고
+                    #현재노드를 삭제
+                    while self.right!=None:
+                        parent=self
+                        self=self.right
+                    temp.data,self.data=self.data,temp.data
+                    parent.right=None
+                    del(self)
+
+    #전위 순회  root->left->right
     def preorder(self):
         def preorder(node):
             if node==None:
@@ -102,6 +171,7 @@ class Btree(object):
                 preorder(self.right)
         print()
 
+    #중위 순회  left->root->right
     def inorder(self):
         def inorder(node):
             if node==None:
@@ -122,7 +192,8 @@ class Btree(object):
                 print(self.data,end="  ")
                 inorder(self.right)
         print()
-            
+          
+    #후위 순회  left->right->root
     def postorder(self):
         def postorder(node):
             if node==None:
@@ -145,19 +216,20 @@ class Btree(object):
         print()
 
 tree=Btree()
-tree.preorder()
+parent=Btree()
+tree.preorder()    #공백 상태 순회시도
+
 tree.insertnode(3)
 tree.insertnode(1)
 tree.insertnode(5)
-#tree.deletenode(5)
 tree.insertnode(0)
-#tree.deletenode(0)
 tree.insertnode(2)
 tree.insertnode(4)
 tree.insertnode(6)
-#tree.insertnode(10)
-#tree.deletenode(10)
-#tree.deletenode(100)
+tree.deletenode(10)  #없는 노드 삭제 시도
+tree.insertnode(10)
+tree.deletenode(10)
+
 tree.preorder()
 tree.inorder()
 tree.postorder()
