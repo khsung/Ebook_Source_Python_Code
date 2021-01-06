4b1374afd2a469dea2ace85556e34fc9c8fe1917
khsung <khsung0@nate.com>
2021-01-06 17:43:51 +0900

    벨만 포드알고리즘

--- Ebook_Source_Python_Code/Bellman-Ford_Algorithm.py
+++ Ebook_Source_Python_Code/Bellman-Ford_Algorithm.py
@@ -1,5 +1,23 @@
 
-inf=float("inf")
-graph=[[]]
-cost=[0]*len(graph)
-print(inf)
+#모든 간선을 확인해야 하는데 edgecount가 필요한가??
+#3중 for문이면 될듯
+def bellman_ford(graph,cost,edgecount,startnode):
+    for i in range(len(graph)):
+        for j in range(edgecount):
+
+
+inf = float("inf")   #무한대 변수 선언
+graph = [[0,6,4,9,inf],[-3,0,inf,1,6],[-3,inf,0,2,inf],[inf,inf,inf,0,4],[inf,2,inf,inf,0]]
+#비용 초기화
+cost = [0] * len(graph)
+edgecount = 0       #간선의 개수
+startnode = 0       #시작 노드
+print("그래프 행렬")
+#그래프 출력하면서 각 노드별 간선의 개수 추가
+for i in range(len(graph)):
+    edgecount+=len(graph[i])
+    edgecount-=graph[i].count(0)     #0은 자기 자신노드
+    edgecount-=graph[i].count(inf)   #inf는 연결이 안된 노드
+    print(graph[i])
+
+bellman_ford(graph,cost,edgecount,startnode)
