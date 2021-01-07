995e27dc8f3e879417f7f7a8bd5f56164c8bfa17
khsung <khsung0@nate.com>
2021-01-07 15:14:43 +0900

    벨만포드알고리즘

--- Ebook_Source_Python_Code/Bellman-Ford_Algorithm.py
+++ Ebook_Source_Python_Code/Bellman-Ford_Algorithm.py
@@ -1,28 +1,32 @@
 
-#모든 간선을 확인해야 하는데 edgecount가 필요한가??
-#3중 for문이면 될듯
-def bellman_ford(graph,cost,edgecount,startnode):
-    #시작 노드를 위한 반복
+
+def bellman_ford(graph,cost,startnode,edgeinfo):
     for i in range(len(graph)):
-        startnode=i
-        for j in range(len(graph)):
-            for k in range(len(graph[j])):
+        for j in range(len(edgeinfo)):
+            curr=edgeinfo[j][0]
+            next=edgeinfo[j][1]
+            edgecost=edgeinfo[j][2]
+            if cost[next]>cost[curr]+edgecost:
+                cost[next]=cost[curr]+edgecost          
+        print(cost)
 
 
 inf = float("inf")   #무한대 변수 선언
 graph = [[0,6,4,9,inf],[-3,0,inf,1,6],[-3,inf,0,2,inf],[inf,inf,inf,0,4],[inf,2,inf,inf,0]]
+#간선 정보 배열 [시작 노드, 도착 노드, 가중치] 형태로 저장 예정
+edgeinfo=[]
 #비용 초기화
-cost = [0] * len(graph)
-#edgecount = 0           #간선의 개수
+cost = [inf] * len(graph)
 startnode = 0           #시작 노드
 cost[startnode] = 0     #시작 노드 비용 갱신
-
 print("그래프 행렬")
-#그래프 출력하면서 각 노드별 간선의 개수 추가
+#그래프 출력하면서 각 노드별 간선 정보 저장
 for i in range(len(graph)):
-    #edgecount+=len(graph[i])
-    #edgecount-=graph[i].count(0)     #0은 자기 자신노드
-    #edgecount-=graph[i].count(inf)   #inf는 연결이 안된 노드
-    print(graph[i])
+    for j in range(len(graph[i])):
+        #print(graph[i][j],end=" ")
+        if graph[i][j] != 0 and graph[i][j] != inf:
+            edgeinfo.append([i, j, graph[i][j]])
+    #print()
+bellman_ford(graph,cost,startnode,edgeinfo)
 
-bellman_ford(graph,cost,edgecount,startnode)
+print(cost)
