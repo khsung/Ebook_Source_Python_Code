7ef7323b8de9b6b422eb72e798ebf690582d230b
khsung <khsung0@nate.com>
2021-01-13 16:36:58 +0900

    힙 정렬

--- Ebook_Source_Python_Code/HeapSort.py
+++ Ebook_Source_Python_Code/HeapSort.py
@@ -5,11 +5,15 @@ num=[6,3,5,7,9,1,6]
 heap=[]
 sorted_num=[]
 print("정렬 전 리스트 : ",num)
+
+#(-값, 값)은 원래 값을 음수 형태로 첫 번째 인덱스에
+#저장하여 오름차순으로 정렬하면 두 번째 인덱스는
+#자동으로 내림차순 형태로 저장되는 원리를 사용한 것
 for i in num:
     heapq.heappush(heap,(-i,i))
 
+#두 번째 인덱스의 값(원래 값)을 정렬 된 리스트에 저장
 while heap:
     sorted_num.append(heapq.heappop(heap)[1])
-
-print("\n정렬 된 리스트 : ",sorted_num)
+print("\n최대 힙으로 정렬한 리스트 : ",sorted_num)
 
