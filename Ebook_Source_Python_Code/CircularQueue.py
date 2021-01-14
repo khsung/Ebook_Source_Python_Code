ea36cde6b7762a8142214b2d55e3df958a2873d0
khsung <khsung0@nate.com>
2021-01-14 15:06:08 +0900

    원형 큐

--- Ebook_Source_Python_Code/CircularQueue.py
+++ Ebook_Source_Python_Code/CircularQueue.py
@@ -1,57 +1,57 @@
 
-class CircularQueue:          #원형 큐 클래스 선언
+class CircularQueue:            #원형 큐 클래스 선언
 
-    def __init__(self, size):
-        self.front=0
-        self.rear=0
-        self.queuesize=size
-        self.data=[None]*size
+    def __init__(self, size):   #원형 큐 초기화
+        self.front = 0
+        self.rear = 0
+        self.queuesize = size
+        self.data = [None] * size
 
-    def isempty(self):       #원형 큐 공백상태 체크
-        if self.front==self.rear:
+    def isempty(self):          #원형 큐 공백상태 체크
+        if self.front == self.rear:
             return True
         else:
             return False
 
-    def isfull(self):        #원형 큐 포화상태 체크
-        if (self.rear+1)%self.queuesize == self.front:
+    def isfull(self):           #원형 큐 포화상태 체크
+        if (self.rear + 1) % self.queuesize == self.front:
             return True
         else:
             return False
 
-    def enqueue(self, data):  #Rear 위치에 원소 삽입
-        if self.isfull()==True:
+    def enqueue(self, data):    #Rear 위치에 원소 삽입
+        if self.isfull() == True:
             print("포화 원형 큐")
         else:
-            self.rear=(self.rear+1)%self.queuesize
+            self.rear = (self.rear + 1) % self.queuesize
             self.data[self.rear] = data
         
-    def dequeue(self):       #Front 다음 위치 원소 제거
+    def dequeue(self):          #Front 다음 위치 원소 제거
         if self.isempty() == True:
             print("공백 원형 큐")
         else:
-            self.front=(self.front+1)%self.queuesize
+            self.front = (self.front + 1) % self.queuesize
             
-    def peek(self):          #Front 다음 위치 원소 출력
+    def peek(self):             #Front 다음 위치 원소(제일 먼저 넣은 원소) 출력
         if self.isempty() == True:
             print("공백 원형 큐")
         else:
-            print("첫 번째 원소 : ",self.data
-                  [(self.front+1)%self.queuesize])
+            print("첫 번째 원소 : ",self.data[(self.front + 1) % self.queuesize])
         
-    def printqueue(self):    #원형 큐 출력
+    def printqueue(self):       #원형 큐 출력
         if self.isempty() == True:
             print("공백 원형 큐")
         else:
-            f=self.front
-            r=self.rear
+            f = self.front
+            r = self.rear
             print("원형 큐 = ",end="")
-            while f%self.queuesize != r%self.queuesize:
-                print(self.data[(f+1)%self.queuesize],end="   ")
-                f=(f+1)%self.queuesize
+            #나머지 연산을 통해 원형으로 돌면서 순서대로 원소 출력
+            while f % self.queuesize != r % self.queuesize:
+                print(self.data[(f + 1) % self.queuesize],end="   ")
+                f = (f + 1) % self.queuesize
             print()
         
-cqueue=CircularQueue(4)
+cqueue = CircularQueue(4)
 cqueue.enqueue(10)
 cqueue.enqueue(20)
 cqueue.peek()
