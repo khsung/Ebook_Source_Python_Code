b2728aebff900acc4e8cc6a68e68073f6458a6a7
khsung <khsung0@nate.com>
2021-01-16 00:08:07 +0900

    이진 검색

--- Ebook_Source_Python_Code/BinarySearch.py
+++ Ebook_Source_Python_Code/BinarySearch.py
@@ -1,22 +1,25 @@
 
 import math     #수학연산을 위한 math모듈
+searchlist = [1, 2, 3, 4, 5, 6, 7]
+left = 0
+right = len(searchlist) - 1
+print("기존 리스트 :",searchlist)
 
-searchlist=[1,2,3,4,5,6,7]
-left=0
-right=len(searchlist)-1
-answer=5        #원하는 원소 값
-check=False     #원소가 있는지 체크하는 변수
-while left<=right:
-    mid=math.floor((right+left)/2)  #중심 인덱스 정수로 계산
-    if searchlist[mid]==answer:
-        print("정답 원소 :",searchlist[mid])
-        print("정답 원소의 인덱스 :",mid)
-        check=True
-        break
-    elif searchlist[mid]>answer:
-        right=mid-1
-    else:
-        left=mid+1
-if check==False:
-    print("해당 원소 없음")
+for i in range(2):
+    check = False     #원소가 있는지 체크하는 변수
+    answer = int(input("찾을 원소 : "))
+    while left <= right:
+        mid = math.floor((right + left) / 2)  #중간 인덱스 정수로 계산
+        if searchlist[mid] == answer:
+            print(searchlist[mid],"검색 완료, 인덱스 :",mid)
+            check = True
+            break
+        #중간 값보다 찾을 값이 더 작을 경우
+        elif searchlist[mid] > answer:
+            right = mid - 1
+        #중간 값보다 찾을 값이 더 클 경우
+        else:
+            left = mid + 1
+    if check == False:
+        print("해당 원소 없음")
 
