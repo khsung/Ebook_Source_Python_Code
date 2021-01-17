fefa63947e053dbfb9624d27af18178aef9db341
khsung <khsung0@nate.com>
2021-01-17 23:40:19 +0900

    삽입 정렬

--- Ebook_Source_Python_Code/InsertionSort.py
+++ Ebook_Source_Python_Code/InsertionSort.py
@@ -1,13 +1,16 @@
 
-data=[2,1,3,5,4]
+data = [2,1,3,5,4]
 print("정렬 전 배열 :",data)
-print("정렬 과정")
+
+print("\n삽입 정렬 과정")
 for i in range(1,len(data)):
-    for j in range(i-1,-1,-1):
-        if data[j]<data[i]:   #앞의 원소보다 크면 중단
+    for j in range(i - 1,-1,-1):
+        #앞의 원소보다 크면 중단
+        if data[j] < data[i]:
             break
-        else:               #앞의 원소보다 작으면 교환
-            data[j],data[j+1]=data[j+1],data[j]
+        #앞의 원소보다 작으면 교환
+        else:
+            data[j],data[j + 1] = data[j + 1],data[j]
     print(data)
-print("정렬 된 배열 :",data)
+print("\n정렬 된 배열 :",data)
 
