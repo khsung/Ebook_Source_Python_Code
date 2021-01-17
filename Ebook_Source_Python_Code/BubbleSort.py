3bc31f52c30522d5d0ea242a9715ef8af1f7894a
khsung <khsung0@nate.com>
2021-01-17 22:58:12 +0900

    버블 정렬

--- Ebook_Source_Python_Code/BubbleSort.py
+++ Ebook_Source_Python_Code/BubbleSort.py
@@ -1,12 +1,12 @@
 
 data=[2,3,1,5,4]
 print("정렬 전 배열 :",data)
-print("정렬 과정")
+print("\n버블 정렬 과정")
 for i in range(len(data),0,-1):
     for j in range(0,i-1):
         #왼쪽 값이 클 경우 원소 교환
         if data[j]>data[j+1]:
             data[j],data[j+1]=data[j+1],data[j]
         print(data)
-print("정렬 후 배열 :",data)
+print("\n정렬 후 배열 :",data)
 
