594afbfe791cbbf77072f6d364a65c9297a01cd1
khsung <khsung0@nate.com>
2021-01-18 10:58:26 +0900

    계수 정렬

--- Ebook_Source_Python_Code/CountingSort.py
+++ Ebook_Source_Python_Code/CountingSort.py
@@ -1,19 +1,18 @@
 
-array=[4,1,0,4,2,1,2]
-countarray=[0,0,0,0,0,0,0]
+array = [4, 1, 0, 4, 2, 1, 2]
+countarray = [0, 0, 0, 0, 0, 0, 0]
 print("정렬 전 리스트 : ",array)
-print("\n정렬과정")
-#리스트 원소값의 
-#countarray 인덱스에 1씩 더하기
+
+print("\n계수 정렬 과정")
+#리스트 원소값의 countarray 인덱스에 1씩 더하기
 for i in range(0,len(array)):
     countarray[array[i]]+=1  
 
-array=[]           #원래 리스트 초기화
-
+array = []                      #원래 리스트 초기화
 for i in range(0,len(countarray)):
-    while countarray[i]>0:  #countarray의 원소가
-        array.append(i)     #0이 될때까지 인덱스값을
-        countarray[i]-=1    #원래 리스트에 추가해줌
+    while countarray[i] > 0:    #countarray의 원소가
+        array.append(i)         #0이 될때까지 인덱스값을
+        countarray[i]-=1        #원래 리스트에 추가
         print(array)
 print("\n정렬 된 리스트 : ",array)
 
