21aac03ce4a50ec03a42479ac08fd45bf00adef0
khsung <khsung0@nate.com>
2021-01-20 11:39:42 +0900

    이중 연결 리스트

--- Ebook_Source_Python_Code/DoubleLinkedList.py
+++ Ebook_Source_Python_Code/DoubleLinkedList.py
@@ -1,56 +1,54 @@
 
-class Node(object):             #Node 클래스 선언
-    def __init__(self, data):   #Node 초기화
-        self.data=data          #Node 키 값
-        self.prev=None
-        self.next=None
+class Node(object):                 #Node 클래스 선언
+    def __init__(self, data):       #Node 초기화
+        self.data = data            #Node 키 값
+        self.prev = None
+        self.next = None
         
-class DoubleLinkedList(object):  #이중 연결 리스트
-                                 #클래스 선언
-    def __init__(self):          #리스트 초기화
-        self.prev=None           #앞 노드
-        self.next=None           #뒤 노드
+class DoubleLinkedList(object):     #이중 연결 리스트 클래스 선언
+    def __init__(self):             #리스트 초기화
+        self.prev = None            #앞 노드
+        self.next = None            #뒤 노드
 
-    def addnode(self,node):      #뒤에 원소 삽입
-        if self.next==None:
-            self.next=node
-        else:
-            curr=self
-            while curr.next!=None:
-                curr=curr.next
-            curr.next=node
-            node.prev=curr
+    def addnode(self,node):         #뒤에 원소 삽입
+        curr = self
+        #다음 노드가 없을 때까지 뒤로 이동
+        while curr.next != None:
+            curr = curr.next
+        #노드 연결
+        curr.next = node
+        node.prev = curr
 
-    def deletenode(self,data):   #특정 원소 제거
-        if self.next==None:
+    def deletenode(self,data):      #특정 원소 제거
+        if self.next == None:
             print("공백 리스트")
         else:
-            while self.next!=None:
+            while self.next != None:
                 #다음 노드가 지울 노드일 때
-                if self.next.data==data:
-                    temp=self.next
-                    self.next=self.next.next
-                    #다음 노드가 마지막 노드가
-                    #아니면 그 다음 노드의
-                    #앞 노드를 현재 노드로 연결
-                    if self.next!=None:
-                        self.next.prev=self
+                if self.next.data == data:
+                    #지울 노드를 임시 저장
+                    temp = self.next
+                    #지울 노드의 next를 저장
+                    self.next = self.next.next
+                    #다음 노드가 존재할 경우 양방향 연결
+                    if self.next != None:
+                        self.next.prev = self
                     del temp
                     return
-                self=self.next
+                self = self.next
         print("해당 노드 없음")
 
-    def printnode(self):         #리스트 출력
-        if self.next==None:
+    def printnode(self):            #리스트 출력
+        if self.next == None:
             print("공백 리스트")
         else:
             print("이중 연결 리스트 = ",end="")
-            while self.next!=None:
+            while self.next != None:
                 print(self.next.data,end="   ")
-                self=self.next
+                self = self.next
             print()
 
-slist=DoubleLinkedList()   #이중 연결 리스트 생성
+slist = DoubleLinkedList()          #이중 연결 리스트 생성
 slist.printnode()
 slist.addnode(Node(10))
 slist.addnode(Node(20))
