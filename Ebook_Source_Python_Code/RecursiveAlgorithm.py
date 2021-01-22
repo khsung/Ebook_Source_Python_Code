10ab3e50b5253bd215c73527556935f9e256b038
khsung <khsung0@nate.com>
2021-01-22 16:36:49 +0900

    재귀 알고리즘

--- Ebook_Source_Python_Code/RecursiveAlgorithm.py
+++ Ebook_Source_Python_Code/RecursiveAlgorithm.py
@@ -1,11 +1,11 @@
 
 def recursive(num):
     if num==0:    #종료 조건
-        print("count 종료")
+        print("sum 종료")
         return 0
     else:
-        print("count =",num)
+        print("num =", num)
         return num+recursive(num-1)
 
-print("총 걸린 시간 :",recursive(5))
+print("총 합 :",recursive(5))
 
